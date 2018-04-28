f1fa18e0be67a89e2c9945274156e9b7957b03d2
yongzx <zhengxin.yong@minerva.kgi.edu>
2018-04-28 11:16:50 +0800

    CLI interface

--- tap.js
+++ tap.js
@@ -1,5 +1,12 @@
+// Customize the reporting
+
 var testCode = require('tape');
 var path = require('path');
+
+var clc = require('cli-color');
+var pass = clc.green;
+var error = clc.red;
+var expect = clc.magenta;
 
 testCode.createStream({ objectMode: true }).on('data', function (eachTest) {
     if (eachTest.type === "test"){
@@ -9,11 +16,11 @@ testCode.createStream({ objectMode: true }).on('data', function (eachTest) {
         console.log("-----End of Test-----")
     }
     else if (eachTest.ok === true){
-        console.log("pass");
+        console.log(pass("pass"));
     }
     else if (eachTest.ok === false){
-        console.log("Your code's output:", eachTest.expected);
-        console.log("Expected output:", eachTest.actual);
+        console.log(error("Your code's output:", eachTest.expected));
+        console.log(expect("Expected output:", eachTest.actual));
     }
     console.log();
 });
