4a41e29e80d414a1f8d2c0097b3d9d7afdf72504
yongzx <zhengxin.yong@minerva.kgi.edu>
2018-04-28 12:51:48 +0800

    Update with Travis

--- code_runner.js
+++ code_runner.js
@@ -1,11 +1,12 @@
 // assign the tape library to the variable "test"
-const testCode = require('tape');
-const unitTest = require("./unit_tests/test_sum");  // Unit tests
-const code = require("./problems/sum");             // Written function
+var testCode = require('tape');
+var unitTest = require("./unit_tests/test_sum");  // Unit tests
+var code = require("./problems/sum");             // Written function
 
 
 testCode(unitTest.testCodeString, { timeout: 10000 }, function (t) {
-    for (index = 0; index < unitTest.testCases.length; ++index){
+    var index;
+    for (index = 0; index < unitTest.testCases.length; index += 1){
         console.log(`Test Case #${index}`, "\nInput(s):", unitTest.testCases[index]);
         t.equal(unitTest.answers[index], code.mainFunc(unitTest.testCases[index][0], unitTest.testCases[index][1]));
     }
