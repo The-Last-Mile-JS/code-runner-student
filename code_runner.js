e06f5951921a2ddf7079b459acbbaed989a0e443
yongzx <zhengxin.yong@minerva.kgi.edu>
2018-04-28 09:32:44 +0800

    Simple working Tape

--- code_runner.js
+++ code_runner.js
@@ -1,6 +1,8 @@
-var test = require('tape'); // assign the tape library to the variable "test"
+// assign the tape library to the variable "test"
+const testCode = require('tape');
 
-test('should return -1 when the value is not present in Array', function (t) {
+const unitTest = require("./unit_tests/test_sum"); // Description of the Tape test output
+testCode(unitTest.testCodeString, { timeout: 10000 }, function (t) {
   t.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so passes
   t.end();
 });
---
+++ unit_tests/test_sum.js
@@ -0,0 +1,3 @@
+module.exports = {
+    testCodeString : "Return the sum of integers" // Description of the Tape test output
+};
