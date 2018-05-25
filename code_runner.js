04cf6a6cb7c5565d1807827fe764f62d4713b77e
yongzx <zhengxin.yong@minerva.kgi.edu>
2018-05-25 14:42:10 +0800

    Update

--- code_runner.js
+++ code_runner.js
@@ -1,14 +1,14 @@
 // assign the tape library to the variable "test"
 var testCode = require('tape');
 var unitTest = require("./unit_test");  // Unit tests
-var code = require("./problem");             // Written function
+var student = require("./problem");             // Written function
 
 
 testCode(unitTest.testCodeString, { timeout: 10000 }, function (t) {
     var index;
     for (index = 0; index < unitTest.testCases.length; index += 1){
         console.log(`Test Case #${index}`, "\nInput(s):", unitTest.testCases[index]);
-        t.equal(unitTest.answers[index], code.mainFunc(unitTest.testCases[index][0], unitTest.testCases[index][1]));
+        t.equal(unitTest.answers[index], student.mainFunc(unitTest.testCases[index][0], unitTest.testCases[index][1]));
     }
     t.end();
 });
